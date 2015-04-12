8cdfe7907d43a788bdfafd52e32203f5e27a5950
Kris-Mikael Krister <kris-mikael.krister@webstep.no>
2015-04-12 17:40:32 +0200

    Correct module dep for mock app

--- test/mock/mock-services.js
+++ test/mock/mock-services.js
@@ -1,4 +1,4 @@
-angular.module('guestbookAppMock', ['guestbookAppMock', 'ngMockE2E'])
+angular.module('guestbookAppMock', ['guestbookApp', 'ngMockE2E'])
 	.run(function ($httpBackend) {
 		$httpBackend.whenGET(/.*/).passThrough();
 		$httpBackend.whenPOST(/.*/).passThrough();
