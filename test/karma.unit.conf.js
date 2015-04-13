722bbf9e966644ace955d48ea5c88210213463b6
Kris-Mikael Krister <kris-mikael.krister@webstep.no>
2015-04-13 21:22:03 +0200

    Update refs to renamed file

--- test/karma.unit.conf.js
+++ test/karma.unit.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
             'bower_components/angular-mocks/angular-mocks.js',
             'bower_components/angular-route/angular-route.js',
             'src/guestbook-app.js',
-            'test/mock/mock-services.js',
+            'test/mock/guestbook-app-mock.js',
             'src/guestbook-app_test.js'
             <!-- endinjector -->
         ],
