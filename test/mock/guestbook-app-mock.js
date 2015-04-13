cf83a8e31ab01da7b2fc019bfe764de97ff0a786
Kris-Mikael Krister <kris-mikael.krister@webstep.no>
2015-04-13 22:41:00 +0200

    Task 5

--- test/mock/guestbook-app-mock.js
+++ test/mock/guestbook-app-mock.js
@@ -11,6 +11,22 @@ angular.module('guestbookAppMock', ['guestbookApp', 'ngMockE2E'])
                 name: 'My other guestbook'
             }
         ]);
+        $httpBackend.whenGET(/\/guestbook\/\d+\/entries/).respond([
+            {
+                id: 1,
+                name: 'Great',
+                message: 'Awesome guestbook',
+                email: 'ole@bru.mm'
+            },
+            {
+                id: 2,
+                name: 'My other guestbook',
+                name: 'Alright...',
+                message: 'It\'s ok',
+                email: 'nasse@no.ff'
+            }
+       ]);
+
         $httpBackend.whenGET(/.*/).passThrough();
 
         $httpBackend.whenPOST(/.*/).passThrough();
